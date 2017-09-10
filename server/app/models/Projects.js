64d3e3b9b980f6faef2e8a3a846d99eb13cb8c28
Akhter <sakhter@corp.intuit.net>
2017-09-10 10:20:04 -0700

    updated formatting

--- server/app/models/Projects.js
+++ server/app/models/Projects.js
@@ -2,40 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let ProjectSchema = new Schema({
-    name:{
-        type:String,
-        require:true
+    name: {
+        type: String,
+        require: true
     },
-    summary:String,
+    summary: String,
     description: String,
-    tech_tags:[],
-    start_date:Date, 
+    tech_tags: [],
+    start_date: Date,
     duration: Number,
     members_wanted: {
-        type:Number,
+        type: Number,
         require: true
     },
-    google_drive_link:String,
+    google_drive_link: String,
     trello_link: String,
-    repo_link:String,
-    deploy_link:String, 
+    repo_link: String,
+    deploy_link: String,
     owner_id: {
-        type:Schema.ObjectId,
-        ref:'Users',
-        require:true
-    }, 
-    cohort_id:{
-        type:Schema.ObjectId, 
-        ref:'Cohorts',
+        type: Schema.ObjectId,
+        ref: 'Users',
         require: true
     },
-    pending_members:[{
-        type:Schema.ObjectId,
-        ref:'Users'
+    cohort_id: {
+        type: Schema.ObjectId,
+        ref: 'Cohorts',
+        require: true
+    },
+    pending_members: [{
+        type: Schema.ObjectId,
+        ref: 'Users'
     }],
-    members:[{
-        type:Schema.ObjectId,
-        ref:'Users'
+    members: [{
+        type: Schema.ObjectId,
+        ref: 'Users'
     }]
 });
-module.exports=mongoose.model('Project', ProjectSchema);
+module.exports = mongoose.model('Project', ProjectSchema);
