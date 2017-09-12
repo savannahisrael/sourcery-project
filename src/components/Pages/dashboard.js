f4d7897869e68bbd03d8e346d3ff0b46ba50edee
bcmiller713 <bcmiller713@gmail.com>
2017-09-12 14:46:52 -0700

    added sample content to components using react semantic ui

---
+++ src/components/Pages/dashboard.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Tab } from 'semantic-ui-react';
+import CreateProjectForm from './createProject';
+import TileGrid from '../Common/tileGrid';
+
+const panes = [
+  { menuItem: 'Active Projects', render: () =>
+    <Tab.Pane attached={false}>
+      <TileGrid/>
+    </Tab.Pane>
+  },
+  { menuItem: 'Past Projects', render: () =>
+    <Tab.Pane attached={false}>
+      <TileGrid/>
+    </Tab.Pane>
+  },
+  { menuItem: 'Create Project', render: () =>
+    <Tab.Pane attached={false}>
+      <CreateProjectForm/>
+    </Tab.Pane>
+  },
+]
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+    );
+  }
+}
+
+export default Dashboard
---
+++ src/components/Pages/explore.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import contentContainer from '../Common/contentContainer';
+import TileGrid from '../Common/tileGrid';
+
+class Explore extends Component {
+  render() {
+    return (
+      <contentContainer>
+        <TileGrid/>
+      </contentContainer>
+    );
+  }
+}
+
+export default Explore;
