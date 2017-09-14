96e9804fe964f01028d098a200aef6caa324519b
Cindy Chen <cindyjan15@gmail.com>
2017-09-14 16:37:09 -0700

    new sticky footer and user profile

--- src/components/Pages/userProfile.js
+++ src/components/Pages/userProfile.js
@@ -1,47 +1,28 @@
+import React, { Component } from "react";
+import { Container, Image, Header, Segment, Grid } from 'semantic-ui-react';
 
-import React, { Component } from 'react'
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Segment,
-  Visibility,
-} from 'semantic-ui-react'
-
-export default class userProfile extends Component {
-
+class Project extends Component {
   render() {
     return (
-      <div>
-
-
-        <Segment
-          textAlign='center'
-          style={{ minHeight: 700, padding: '1em 0em' }}
-          vertical
-        >
-          <Container text>
-            <Image src='/assets/images/wireframe/square-image.png' size='medium' shape='circular' />
-            <Header
-              as='h1'
-              content='Cindy Chen'
-              style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '1.5em' }}
-            />
-            <Header
-              as='h2'
-              content='Cindygchen'
-              style={{ fontSize: '1.5em', fontWeight: 'normal' }}
-            />
-          </Container>
-        </Segment>
-      </div>
-    )
+    <div>
+        <br/><br/>
+        <Grid centered columns={3}>
+            <Segment>
+                <Image src='http://lorempixel.com/output/cats-q-c-480-480-1.jpg' size='small' shape='circular' centered />
+                <Header textAlign='center' as='h1'>
+                Cindy Chen
+                    <Header.Subheader>
+                        www.cindygchen.com
+                    </Header.Subheader>
+                </Header>
+                <Container text textAlign='center'>
+                <p>Bio goes here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium.</p>
+                </Container>
+            </Segment>
+        </Grid>
+    </div>
+    );
   }
 }
 
+export default Project
