188d9a5989aebf940cb86a17a9891d427a302a9a
bcmiller713 <bcmiller713@gmail.com>
2017-09-15 22:59:18 -0700

    moved inline styles to css files

--- src/components/Common/footer/footer.js
+++ src/components/Common/footer/footer.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Sticky, Segment, Container, Divider, Image, List, Grid, Header } from 'semantic-ui-react';
+import "./footer.css";
 
 const footer = () => (
   <div>
-    <Segment
-          inverted
-          style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
-          vertical
-        >
-          <Container textAlign='center'>
-            <Image src='/logo.png' centered size='mini' />
-            <p>
-              Made in San Diego © devCircle 2017
-            </p>
-          </Container>
-        </Segment>
+    <Segment inverted vertical>
+      <Container className='footer' textAlign='center'>
+        <Image src='/logo.png' centered size='mini' />
+        <p>Made in San Diego © devCircle 2017</p>
+      </Container>
+    </Segment>
   </div>
 )
 
