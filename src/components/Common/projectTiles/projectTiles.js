d79efd7c08c64bd656ad65f68fe085aee6c23efa
bcmiller713 <bcmiller713@gmail.com>
2017-09-18 20:32:05 -0700

    added sample data for projects, started passing props from explore.js into into projectTiles.js

--- src/components/Common/projectTiles/projectTiles.js
+++ src/components/Common/projectTiles/projectTiles.js
@@ -2,21 +2,20 @@ import React from 'react';
 import { Card, Icon, Image, Button, Label } from 'semantic-ui-react';
 import './projectTiles.css';
 
-const Tile = () => (
+const Tile = (props) => (
   <Card raised>
-
     <Card.Content className='tileHeader'>
       <Card.Header className='tileTitle' >
-        Project Title Goes Here and Second Line If Needed
+        {props.title}
       </Card.Header>
       <i className="devicon-python-plain iconStyle"></i>
-      {/*<svg className='svgicon' viewBox="0 0 80 80" width="48" height="48" fill="#FFFFFF"  xmlns="http://www.w3.org/2000/svg" aria-labelledby="title"><title>Python Icon</title><path d="M71.13 20.13H59.87V8.87L51 0H29l-8.87 8.87v11.26H8.87L0 29v22l8.87 8.87h11.26v11.26L29 80h22l8.87-8.87V59.87h11.26L80 51V29zM10.75 55.35l-6.22-6.22V30.87l6.22-6.22H40v-4.52H24.65v-9.38l6.22-6.22h18.26l6.22 6.22v18.16l-8.75 8.83H31.52L20.13 49.13v6.22zm64.72-6.22l-6.22 6.22H40v4.53h15.35v9.38l-6.22 6.22H30.87l-6.22-6.22V51l8.74-8.74h15.1l11.38-11.5v-6.11h9.38l6.22 6.22z"></path><path d="M30.19 9.81h4.53v5.03h-4.53zM45.28 65.16h4.53v5.03h-4.53z"></path></svg>*/}
        <Card.Meta className='tileState'>
-          Sept 16 | 3 weeks
+          {props.start_date} | {props.duration} Days
       </Card.Meta>
     </Card.Content>
     <Card.Content className='tileContent'>
-        <Label className='tileTags'>
+        {props.renderTechTags}
+        {/* <Label className='tileTags'>
           HTML/CSS
         </Label>
         <Label className='tileTags'>
@@ -33,9 +32,9 @@ const Tile = () => (
         </Label>
         <Label className='tileTags'>
           Frontend
-        </Label>
+        </Label> */}
       <Card.Description>
-        An app for the continuing education of coding boot camp graduates
+        {props.summary}
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
@@ -49,9 +48,7 @@ const Tile = () => (
           Creator
         </Label>
     </Card.Content>
-        <Button animated='vertical' fluid
-      attached='bottom'
-      className='tileJoin'>
+    <Button animated='vertical' fluid attached='bottom' className='tileJoin' onClick={props.handleJoinButton}>
       <Button.Content visible>
         Join the team
       </Button.Content>
--- src/components/Pages/explore/explore.js
+++ src/components/Pages/explore/explore.js
@@ -1,11 +1,54 @@
-import React, { Component } from "react";
-import { Container, Divider, Header } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Container, Divider, Header, Label } from 'semantic-ui-react';
 import contentContainer from '../../Common/contentContainer';
 import TileGrid from '../../Common/tileGrid';
-import Navbar from "../../Common/navbar";
+import Tile from '../../Common/projectTiles';
+import Navbar from '../../Common/navbar';
 import './explore.css';
+import projectData from '../../../utils/sampleData/sampleProjects.json';
 
 class Explore extends Component {
+
+  state = {
+    filters: [],
+    projects: projectData,
+  };
+
+  // A helper method for rendering one Tile for each project
+  renderTiles = () => {
+    return this.state.projects.map(project => (
+      <Tile
+        // _id={project._id}
+        // key={project._id}
+        title={project.name}
+        summary={project.summary}
+        description={project.description}
+        tech={project.tech_tags}
+        start_date={project.start_date}
+        duration={project.duration}
+        members_wanted={project.members_wanted}
+        google_drive_link={project.google_drive_link}
+        trello_link={project.trello_link}
+        repo_link={project.repo_link}
+        deploy_link={project.deploy_link}
+        renderTechTags={this.renderTechTags}
+        handleJoinButton={this.handleJoinButton}
+      />
+    ));
+  }
+
+  renderTechTags = () => {
+    return this.state.projects.tech_tags.map(tech_tag => (
+      <Label className='tileTags'>
+        {tech_tag}
+      </Label>
+    ));
+  }
+
+  handleJoinButton = () => {
+
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +56,7 @@ class Explore extends Component {
         <Divider/>
         <Header as='h4'>All Projects</Header>
         <Divider/>
-        <TileGrid/>
+        {this.renderTiles()}
       </div>
     );
   }
