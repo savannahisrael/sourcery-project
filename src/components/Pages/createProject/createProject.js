66104384b6431500f8a813d7b1360bc6caf06f77
bcmiller713 <bcmiller713@gmail.com>
2017-09-29 12:32:29 -0700

    bug fixing for filtering on explore

--- src/components/Pages/createProject/createProject.js
+++ src/components/Pages/createProject/createProject.js
@@ -114,16 +114,16 @@ class CreateProjectForm extends Component {
     if (this.state.projectNameInput === '' || this.state.mainTechnologyInput === '' || this.state.membersWantedInput === '') {
       warning.innerHTML = '*Please complete all required fields';
       warning.style.display = 'block';
-      for (let i = 0; i < asterisk.length; i++) {
-        asterisk[i].style.display = 'block';
-      }
+      // for (let i = 0; i < asterisk.length; i++) {
+      //   asterisk[i].style.display = 'block';
+      // }
     }
     else if (this.state.projectNameInput.search(regex) === -1) {
       warning.innerHTML = '*Project title may only contain letters, numbers, dashes, and underscores';
       warning.style.display = 'block';
-      for (let i = 0; i < asterisk.length; i++) {
-        asterisk[0].style.display = 'block';
-      }
+      // for (let i = 0; i < asterisk.length; i++) {
+      //   asterisk[0].style.display = 'block';
+      // }
     }
     else {
       axios.post('/api/projectNew', {
@@ -161,18 +161,15 @@ class CreateProjectForm extends Component {
         <Container className='createContainer'>
           <Segment>
           <Form size='large' class='form' onSubmit={this.handleSubmitButton}>
-            <span className='asterisk'>*</span>
             <Form.Select label='Project Status' options={projectOptions} onChange={this.handleMembersWantedChange}/>
-            <Form.Input label='Project Name' onChange={this.handleprojectNameChange}/>
+            <Form.Input label='Project Name *' onChange={this.handleprojectNameChange}/>
             <Form.Input label='Start Date' placeholder='MM/DD/YYYY' onChange={this.handleStartDateChange}/>
             <Form.Input label='Project Length' placeholder='in weeks' onChange={this.handleProjectDurationChange}/>
             <Form.TextArea label='Project Summary' placeholder='Keep it short and sweet' onChange={this.handleProjectSummaryChange}/>
-            <span className='asterisk'>*</span>
-            <Form.Select search label='Main Technology' options={techSelection} placeholder='e.g. JavaScript' onChange={this.handleMainTechnologyChange}/>
+            <Form.Select search label='Main Technology *' options={techSelection} placeholder='e.g. JavaScript' onChange={this.handleMainTechnologyChange}/>
             <Form.Select multiple search label='Other Technologies' options={techSelection} onChange={this.handleOtherTechnologiesChange}/>
             <Form.TextArea label='Project Details' placeholder='Describe your project in detail...' onChange={this.handleProjectDetailsChange}/>
-            <span className='asterisk'>*</span>
-            <Form.Select label='Team Size' options={memberOptions} onChange={this.handleMembersWantedChange}/>
+            <Form.Select label='Team Size *' options={memberOptions} onChange={this.handleMembersWantedChange}/>
             <Form.Input label='Google Drive Link' onChange={this.handleGoogleLinkChange}/>
             <Form.Input label='Trello Link' onChange={this.handleTrelloLinkChange}/>
             <Form.Input label='Github Link' onChange={this.handleRepoLinkChange}/>
