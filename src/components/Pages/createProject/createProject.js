3a0e3213e14c1d9506c5b0bd07df6616110d529a
fbrahman <fbrahman@gmail.com>
2017-09-29 20:44:54 -0700

    create page bug squashed

--- src/components/Pages/createProject/createProject.js
+++ src/components/Pages/createProject/createProject.js
@@ -6,12 +6,12 @@ import techSelection from '../../../utils/techTags.json';
 import axios from 'axios';
 
 const memberOptions = [
-  { key: '1', text: '1', value: '1' },
-  { key: '2', text: '2', value: '2' },
-  { key: '3', text: '3', value: '3' },
-  { key: '4', text: '4', value: '4' },
-  { key: '5', text: '5', value: '5' },
-  { key: '6', text: '6', value: '6' }
+  { key: '1', text: '1', value: 1 },
+  { key: '2', text: '2', value: 2 },
+  { key: '3', text: '3', value: 3 },
+  { key: '4', text: '4', value: 4 },
+  { key: '5', text: '5', value: 5 },
+  { key: '6', text: '6', value: 6 }
 ]
 
 const projectOptions = [
@@ -86,16 +86,16 @@ class CreateProjectForm extends Component {
     this.setState({ mainTechnologyInput: value});
   }
 
-  handleOtherTechnologiesChange = (event) => {
-    this.setState({ otherTechnologiesInput: event.target.value });
+  handleOtherTechnologiesChange = (e, {value}) => {
+    this.setState({ otherTechnologiesInput: value });
   }
 
   handleProjectDetailsChange = (event) => {
     this.setState({ projectDetailsInput: event.target.value });
   }
 
-  handleMembersWantedChange = (event) => {
-    this.setState({ membersWantedInput: event.target.value });
+  handleMembersWantedChange = (e, {value}) => {
+    this.setState({ membersWantedInput: value });
   }
 
   handleGoogleLinkChange = (event) => {
