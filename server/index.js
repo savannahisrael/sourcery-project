416d93b033548e594c503863f92376c2a24fcdae
Savannah Israel <sisrael0405@gmail.com>
2019-12-14 00:28:03 -0700

    testing again

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 //=============== INITIALIZE EXPRESS APP & SETUP FOR DATA PARSING===============//
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
